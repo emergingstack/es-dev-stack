4119ec20562517d73a09e4fe7f3de826552a3d78
Luke Benson <luke.benson@emergingstack.com>
2016-01-06 15:07:19 +1100

    Preliminary setup items

---
+++ corenvidiadrivers/mkdevs.sh
@@ -0,0 +1,21 @@
+
+if [ "$?" -eq 0 ]; then
+	# Count the number of NVIDIA controllers found.
+	NVDEVS=`lspci | grep -i NVIDIA`
+	N3D=`echo "$NVDEVS" | grep "3D controller" | wc -l`
+	NVGA=`echo "$NVDEVS" | grep "VGA compatible controller" | wc -l`
+	N=`expr $N3D + $NVGA - 1`
+
+	for i in `seq 0 $N`; do
+		mknod -m 666 /dev/nvidia$i c 195 $i
+	done
+	
+	mknod -m 666 /dev/nvidiactl c 195 255
+
+        # Find out the major device number used by the nvidia-uvm driver
+        D=`grep nvidia-uvm /proc/devices | awk '{print $1}'`
+        mknod -m 666 /dev/nvidia-uvm c $D 0
+else
+	exit 1
+fi
+
